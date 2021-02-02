df1482843e663883115a6dfb85675cd19ab5de80
shoothzj <shooothzj@gmail.com>
2021-02-02 22:30:17 +0800

    Zookeeper 不可写代码优化

--- demo-agent/src/main/java/com/github/shoothzj/demo/agent/interceptor/ZooKeeperWriteInterceptor.java
+++ demo-agent/src/main/java/com/github/shoothzj/demo/agent/interceptor/ZooKeeperWriteInterceptor.java
@@ -1,7 +1,8 @@
 package com.github.shoothzj.demo.agent.interceptor;
 
+import com.github.shoothzj.demo.agent.config.ZooKeeperConfigHolder;
 import com.github.shoothzj.demo.agent.util.AgentUtil;
-import com.github.shoothzj.demo.agent.util.ZooKeeperUtil;
+import com.github.shoothzj.demo.agent.config.ZooKeeperConfig;
 import net.bytebuddy.asm.Advice;
 
 /**
@@ -10,17 +11,14 @@ import net.bytebuddy.asm.Advice;
 public class ZooKeeperWriteInterceptor {
 
     /**
-     * #t Class名 ex: com.github.shoothzj.demo.agent.test.controller.TestController
-     * #m Method名 ex: createJob
-     * #d Method描述 ex: (Ljava/lang/String;Lcom/github/shoothzj/demo/agent/test/module/rest/CreateJobReqDto;)Lorg/springframework/http/ResponseEntity;
-     * #s 方法签名 ex: (java.lang.String,com.github.shoothzj.demo.agent.test.module.rest.CreateJobReqDto)
-     * #r 返回类型 ex: org.springframework.http.ResponseEntity
+     * #t Class名 ex: org.apache.zookeeper.server.PrepRequestProcessor
+     * #m Method名 ex: pRequest2Txn
      *
      * @param signature
      */
     @Advice.OnMethodEnter
     public static void enter(@Advice.Origin("#t #m") String signature) {
-        if (ZooKeeperUtil.getWriteError()) {
+        if (!ZooKeeperConfigHolder.isWritable()) {
             throw new IllegalStateException("error " + signature);
         }
         AgentUtil.info("[{}]", signature);
