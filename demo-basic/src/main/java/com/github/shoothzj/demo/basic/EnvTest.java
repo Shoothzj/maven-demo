6885ef51a79e0d691de1aea5ecf91e07f7799302
shoothzj <shooothzj@gmail.com>
2021-02-28 19:52:28 +0800

    bookkeeper demo提交

--- demo-basic/src/main/java/com/github/shoothzj/demo/basic/EnvTest.java
+++ demo-basic/src/main/java/com/github/shoothzj/demo/basic/EnvTest.java
@@ -11,6 +11,8 @@ public class EnvTest {
     public static void main(String[] args) {
         final String property = System.getProperty("user.home");
         System.out.println(property);
+        final String unExistVariable = System.getenv("UN_EXIST_VARIABLE");
+        System.out.println(unExistVariable);
     }
 
 }
